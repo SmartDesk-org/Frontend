c911c52aad052ee7c4bb41872a022410cc84b70e
Muhammed Suhail P <suhailpalakkal1@gmail.com>
2026-01-07 18:42:53 +0530

    some changes in Auth

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -24,12 +24,15 @@ export default function LoginForm() {
     dispatch(login({ email, password }))
       .unwrap()
       .then((res) => {
-        console.log("🟢 LOGIN SUCCESS:", res);
-        if(res?.data.role==1)
-        navigate('/super-admin');
-        else if(res?.data.role==2)
-          navigate("/company-admin");
-      })
+          const role = res?.data?.role;
+
+          if (role === 1) {
+            navigate("/super-admin");
+          } else if (role === 2) {
+            navigate("/company-admin");
+          }
+        })
+
       .catch((err) => {
         console.error("🔴 LOGIN FAILED:", err);
       });
