d9da3298a94c9159aaf6fc31b965a2e27e481523
Muhammed Suhail P <suhailpalakkal1@gmail.com>
2026-01-08 11:05:26 +0530

    Fixed some logic

--- src/redux/api/axiosClient.js
+++ src/redux/api/axiosClient.js
@@ -5,7 +5,7 @@ import { getAuthToken, setAuthToken, clearAuthToken } from "../authToken";
 
 const axiosClient = axios.create({
 
-baseURL: "http://localhost:5210/api",
+baseURL: "https://localhost:7046/api",
 
   withCredentials: true
 });
